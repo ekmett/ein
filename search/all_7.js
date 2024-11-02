6e8e6587108fb7d2c3cc3938fa3f71683d6799c5
ekmett <ekmett@users.noreply.github.com>
2024-11-02 07:59:43 +0000

    deploy: e8c6e7dada0f42a54669d2bfc201fc4fc0ac95ea

--- search/all_7.js
+++ search/all_7.js
@@ -1,15 +1,15 @@
 var searchData=
 [
   ['back_0',['back',['../classein_1_1basic__static__string.html#ac2537a8fb97af9b73b538fb82d533d80',1,'ein::basic_static_string']]],
-  ['ban_1',['Ban',['../code_of_conduct_page.html#autotoc_md16',1,'3. Temporary Ban'],['../code_of_conduct_page.html#autotoc_md17',1,'4. Permanent Ban']]],
+  ['ban_1',['Ban',['../code_of_conduct_page.html#autotoc_md8',1,'3. Temporary Ban'],['../code_of_conduct_page.html#autotoc_md9',1,'4. Permanent Ban']]],
   ['basic_5fstatic_5fstring_2',['basic_static_string',['../classein_1_1basic__static__string.html',1,'ein::basic_static_string&lt; CharT, Traits &gt;'],['../classein_1_1basic__static__string.html#a3c4a58df5bbb2c7e22e9d0fd55838c9c',1,'ein::basic_static_string::basic_static_string(std::basic_string_view&lt; CharT, Traits &gt; view)'],['../classein_1_1basic__static__string.html#a1251d2b1680e6dc9600ff9da18ba5c6a',1,'ein::basic_static_string::basic_static_string(std::integer_sequence&lt; CharT, xs... &gt;)'],['../classein_1_1basic__static__string.html#a9f46dbc43e3dee783626403c045a1ccc',1,'ein::basic_static_string::basic_static_string() noexcept=default'],['../classein_1_1basic__static__string.html#a4bf56601bb0248d3949764a63b01b76c',1,'ein::basic_static_string::basic_static_string(basic_static_string const &amp;) noexcept=default'],['../classein_1_1basic__static__string.html#a5c5bdab7f6023c5321d8e041c08b557a',1,'ein::basic_static_string::basic_static_string(basic_static_string const &amp;&amp;) noexcept=default']]],
   ['basic_5fstatic_5fstring_3c_20char_20_3e_3',['basic_static_string&lt; char &gt;',['../classein_1_1basic__static__string.html',1,'ein']]],
   ['begin_4',['begin',['../group__simd.html#ad3060907ab6bf5d9364fd0403bac506e',1,'ein::simd::begin() noexcept'],['../group__simd.html#a13b00a131976b1dfd9ccf4946c005ea2',1,'ein::simd::begin() const noexcept'],['../classein_1_1basic__static__string.html#abd18393b447e3aa963d5433c7de1534a',1,'ein::basic_static_string::begin()'],['../classein_1_1static__c__string.html#a75f47cf49c773e17083ad66b3c58fc07',1,'ein::static_c_string::begin()']]],
   ['bf16_5',['bf16',['../structein_1_1bf16.html',1,'ein::bf16'],['../structein_1_1bf16.html#ad1ea01a9318dc2b0cee606c203a301fc',1,'ein::bf16::bf16() noexcept=default'],['../structein_1_1bf16.html#ac5f9c503ec58e5958397d9978a6275da',1,'ein::bf16::bf16(float f) noexcept'],['../structein_1_1bf16.html#a1756f7f285ffe6ae802739619f49231a',1,'ein::bf16::bf16(__bf16 f) noexcept'],['../structein_1_1bf16.html#a1d1bde135328a7c30654aadd43825fde',1,'ein::bf16::bf16(bf16 const &amp;) noexcept=default'],['../structein_1_1bf16.html#a0d16d474530be6136b9145a861f0b892',1,'ein::bf16::bf16(bf16 &amp;&amp;) noexcept=default']]],
   ['bf16_2ecpp_6',['bf16.cpp',['../bf16_8cpp.html',1,'']]],
   ['bf16_2ehpp_7',['bf16.hpp',['../bf16_8hpp.html',1,'']]],
-  ['bill_20of_20materials_8',['Software Bill of Materials',['../contributing_page.html#autotoc_md6',1,'']]],
+  ['bill_20of_20materials_8',['Software Bill of Materials',['../contributing_page.html#autotoc_md17',1,'']]],
   ['bp_9',['bp',['../namespaceein_1_1profiling.html#a4fd0fbcb9a46b54e58f8a779e988270b',1,'ein::profiling::profile_event']]],
-  ['bsd_202_20clause_20license_10',['BSD 2-Clause License',['../license_page.html#autotoc_md0',1,'']]],
+  ['bsd_202_20clause_20license_10',['BSD 2-Clause License',['../license_page.html#autotoc_md11',1,'']]],
   ['bytesize_11',['bytesize',['../group__simd.html#af34fa79e65d1522f29b99af1a45f8dd7',1,'ein::simd']]]
 ];
